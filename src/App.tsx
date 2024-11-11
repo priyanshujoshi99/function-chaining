73374e27c244534a8a3021965e7a28dc37184675
Priyanshu Joshi <joshipriyanshu2029@gmail.com>
2024-11-11 22:24:27 +0530

    Added InputNumber component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import FunctionCard from './components/FunctionCard';
+import InputNumber from './components/InputNumber';
 
 const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <FunctionCard
-        cardTitle="Function: 1"
-        equation="2x+4"
-        nextFunction="Function 2"
-      />
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <FunctionCard
+          cardTitle="Function: 1"
+          equation="2x+4"
+          nextFunction="Function 2"
+        />
+      </div>
+
+      <InputNumber type="output" value={21} />
+
+      <InputNumber type="input" />
+    </>
   );
 };
 
---
+++ src/components/InputNumber/InputNumber.tsx
@@ -0,0 +1,65 @@
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import styles from './InputNumber.module.css';
+
+interface InputNumberProps {
+  type?: 'input' | 'output';
+  value?: number;
+}
+
+const InputNumber: React.FC<InputNumberProps> = ({
+  type = 'input',
+  value: propValue
+}) => {
+  const [value, setValue] = useState(2);
+
+  const isOutput = type === 'output';
+  const labelText = isOutput ? 'Final Output y' : 'Initial value of x';
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isOutput) {
+      setValue(Number(event.target.value));
+    }
+  };
+
+  return (
+    <section className={styles.container} aria-live="polite">
+      <label
+        htmlFor="inputNumberField"
+        className={classNames(styles.label, {
+          [styles.outputLabel]: isOutput,
+          [styles.inputLabel]: !isOutput
+        })}
+      >
+        {labelText}
+      </label>
+      {isOutput ? (
+        <div className={styles.outputContainer}>
+          <div className={styles.circle} aria-hidden="true"></div>
+          <input
+            id="inputNumberField"
+            type="number"
+            value={propValue}
+            onChange={handleInputChange}
+            readOnly
+            className={styles.output}
+            aria-readonly={isOutput}
+          />
+        </div>
+      ) : (
+        <div className={styles.inputContainer}>
+          <input
+            id="inputNumberField"
+            type="number"
+            value={value}
+            onChange={handleInputChange}
+            className={styles.input}
+          />
+          <div className={styles.circle} aria-hidden="true"></div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default InputNumber;
