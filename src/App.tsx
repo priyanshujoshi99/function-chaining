4811319c91e04f406329cc9b4c48d6db8c0a35b4
Priyanshu Joshi <joshipriyanshu2029@gmail.com>
2024-11-12 21:03:28 +0530

    Refactored App and added a screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import FunctionCard from './components/FunctionCard';
-import InputNumber from './components/InputNumber';
-import { useCalculateValue } from './hooks/useCalculateValue';
-
-import styles from './App.module.css';
+import CalculatorScreen from './screens/CalculatorScreen';
 
 const App = () => {
-  const value = useCalculateValue();
-
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <InputNumber type="input" className={styles.input} />
-          <div className={styles.cardRow}>
-            <FunctionCard cardTitle="Function: 1" nextFunction="Function: 2" />
-            <FunctionCard cardTitle="Function: 2" nextFunction="Function: 4" />
-            <FunctionCard cardTitle="Function: 3" nextFunction="-" />
-          </div>
-          <InputNumber type="output" value={value} className={styles.output} />
-        </div>
-
-        <div className={styles.row}>
-          <div className={styles.cardRow}>
-            <FunctionCard cardTitle="Function: 4" nextFunction="Function: 5" />
-            <FunctionCard cardTitle="Function: 5" nextFunction="Function: 3" />
-          </div>
-        </div>
-      </div>
+      <CalculatorScreen />
     </>
   );
 };
