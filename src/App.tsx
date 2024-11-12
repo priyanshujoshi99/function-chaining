721d918bf1b781bd697c9a635d8b6ff75d061d85
Priyanshu Joshi <joshipriyanshu2029@gmail.com>
2024-11-12 20:15:38 +0530

    [WIP] Layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,21 @@ const App = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.row1}>
+        <div className={styles.row}>
           <InputNumber type="input" className={styles.input} />
-          <FunctionCard cardTitle="Function: 1" nextFunction="Function: 2" />
-          <FunctionCard cardTitle="Function: 2" nextFunction="Function: 4" />
-          <FunctionCard cardTitle="Function: 3" nextFunction="-" />
+          <div className={styles.cardRow}>
+            <FunctionCard cardTitle="Function: 1" nextFunction="Function: 2" />
+            <FunctionCard cardTitle="Function: 2" nextFunction="Function: 4" />
+            <FunctionCard cardTitle="Function: 3" nextFunction="-" />
+          </div>
           <InputNumber type="output" value={value} className={styles.output} />
         </div>
-        <div className={styles.row2}>
-          <FunctionCard cardTitle="Function: 4" nextFunction="Function: 5" />
-          <FunctionCard cardTitle="Function: 5" nextFunction="Function: 3" />
+
+        <div className={styles.row}>
+          <div className={styles.cardRow}>
+            <FunctionCard cardTitle="Function: 4" nextFunction="Function: 5" />
+            <FunctionCard cardTitle="Function: 5" nextFunction="Function: 3" />
+          </div>
         </div>
       </div>
     </>
