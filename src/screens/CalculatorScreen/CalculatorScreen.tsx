7825df47284fe1ca6b831b72e5710afae66d013b
Priyanshu Joshi <joshipriyanshu2029@gmail.com>
2024-11-13 19:58:54 +0530

    Added connector one

--- src/screens/CalculatorScreen/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen/CalculatorScreen.tsx
@@ -12,9 +12,12 @@ const CalculatorScreen = () => {
       <div className={styles.container}>
         <div className={styles.row}>
           <InputNumber type="input" className={styles.input} />
+          <div className={styles.connectorOne} />
           <div className={styles.cardRow}>
             <FunctionCard cardTitle="Function: 1" nextFunction="Function: 2" />
+            <div className={styles.connectorTwo} />
             <FunctionCard cardTitle="Function: 2" nextFunction="Function: 4" />
+            <div className={styles.connectorThree} />
             <FunctionCard cardTitle="Function: 3" nextFunction="-" />
           </div>
           <InputNumber type="output" value={value} className={styles.output} />
@@ -23,7 +26,9 @@ const CalculatorScreen = () => {
         <div className={styles.row}>
           <div className={styles.cardRow}>
             <FunctionCard cardTitle="Function: 4" nextFunction="Function: 5" />
+            <div className={styles.connectorFour} />
             <FunctionCard cardTitle="Function: 5" nextFunction="Function: 3" />
+            <div className={styles.connectorFive} />
           </div>
         </div>
       </div>
